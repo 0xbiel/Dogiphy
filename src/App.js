e829dc2e9661653e84fee76204fa67ca45bf9a24
Gabriel Polastrini <bielp07@gmail.com>
2019-08-28 14:06:31 -0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Dogiphy 🐶</h1>
+	<h2>the best dog gifs you have ever seen!</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
